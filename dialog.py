90caa60e30546c175de48a0ec446bb0e005e3de2
vasav <sylvain.clesse@student.uclouvain.be>
2022-01-18 15:47:26 +0100

    gang

--- dialog.py
+++ dialog.py
@@ -5,6 +5,7 @@ class DialogBox:
 
     def __init__(self):
         self.box = pygame.image.load("./dialogs/dialog_box.png")
+        self.box = pygame.transform.scale(self.box, (700, 100))
 
     def render(self, screen):
-        screen.blit(self.box, (0, 0))
+        screen.blit(self.box, (60, 680))
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import pygame
+
 from game import Game
 
 if __name__ == '__main__':
-    # pygame.init()
+    pygame.init()
     game = Game()
     game.run()
