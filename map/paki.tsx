ec2d05c2252ae0e4638b95482e9ed432dea6f325
louiscolinet <louiscolinet@gmail.com>
2022-02-18 14:58:49 +0100

    paki

---
+++ map/paki.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="paki" tilewidth="16" tileheight="16" tilecount="30" columns="5">
+ <image source="paki.png" trans="000000" width="83" height="100"/>
+</tileset>
